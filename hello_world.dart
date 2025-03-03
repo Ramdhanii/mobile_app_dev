5f85b722a4fc4fc399857bae418a816fe2655c00
Ramdhanii <47031309+Ramdhanii@users.noreply.github.com>
2025-03-03 11:38:35 +0700

    add first file dart

---
+++ hello_world.dart
@@ -0,0 +1,22 @@
+void main(){
+    print('Hello World');
+    //deklarasi tipe data
+    String nama ='Budi';
+    int umur = 25;
+    print(nama);
+    print(umur);
+
+    var name = 'jhon';
+    print(name);
+
+    final lastName = 'Doe';
+    print(lastName);
+
+    /* Ini adalah contoh 
+    komentar untuk beberapa kode sekaligus dan multiline*/
+    const PI = 3.14;
+    print(PI);
+    late String address;
+    address = 'JL ABC';
+    print(address);
+}
