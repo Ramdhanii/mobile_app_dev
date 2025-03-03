cb91ccd0fa970721c772fd8ca1ad52924056ae8c
Ramdhanii <47031309+Ramdhanii@users.noreply.github.com>
2025-03-03 11:40:10 +0700

    add number.dart

---
+++ number.dart
@@ -0,0 +1,25 @@
+void main() {
+    int number1 = 20;
+    int number2 = 10;
+    double number3 = 10.5;
+    double number4 = 20.5;
+    num number5 = 40;
+    number5 = 40.5;
+    num number6 = 50.5;
+    number6 = 50;
+
+    bool benar = true;
+    bool salah = false;
+
+    print(benar);
+    print(salah);
+
+    print(number1);
+    print(number2);
+    print(number3);
+    print(number4);
+    print(number5);
+    print(number6);
+    print(number1 + number2);
+    print(number3 + number4);
+}
